1fbcf32303f1d20b015d904ceb68c961f530382a
Arijit Das <arijit10das2002@gmail.com>
2022-08-29 13:12:53 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <button className="btn btn-primary" onClick={signInWithGoogle}>Sign In</button>
+      <h2>{localStorage.getItem('name')}</h2>
+      <img src={localStorage.getItem('picture')} alt="pic" />
     </div>
   );
 }
