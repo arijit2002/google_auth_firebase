4d9804996524bdb42afd0ba5b07444d9026320d8
Arijit Das <arijit10das2002@gmail.com>
2022-08-29 12:31:39 +0530

    f

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyA0M0cNl_JE4kyJuSVhEbdoEwD_fYSVHDw",
+  authDomain: "basics-6f1a6.firebaseapp.com",
+  projectId: "basics-6f1a6",
+  storageBucket: "basics-6f1a6.appspot.com",
+  messagingSenderId: "412786801920",
+  appId: "1:412786801920:web:2ded0b318c616bd23847e5",
+  measurementId: "G-T8RQZYZHG1"
 };
 
 // Initialize Firebase
